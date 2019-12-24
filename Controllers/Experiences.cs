aea963a1585b37bb4caaf76a8041e5dcde7212d0
Seyed Mohammad Hadi Tabatabaei <hadi.tabatabaei.aut@gmail.com>
2019-12-24 23:47:55 +0330

    init

---
+++ Controllers/Experiences.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class Experiences : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Portfolio.Models;
+
+namespace Portfolio.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly ILogger<ProfileController> _logger;
+
+        public ProfileController(ILogger<ProfileController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
